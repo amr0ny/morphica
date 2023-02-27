19e7c8ac7604a04f905f5b578f6b9eaf3d1db8c4
Alex <amrany@yandex.ru>
2023-02-27 13:49:04 +0300

    CatmullRomShape class updated

--- core/src/com/iclustudio/morphica/CatmullRomShape.java
+++ core/src/com/iclustudio/morphica/CatmullRomShape.java
@@ -1,6 +1,8 @@
 package com.iclustudio.morphica;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.PolygonRegion;
@@ -9,13 +11,22 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.CatmullRomSpline;
 import com.badlogic.gdx.math.EarClippingTriangulator;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
+import java.util.Random;
 
 public class CatmullRomShape {
 
 
     private static final int POINTS_NUM = 1000;
+
+    private int sides;
+    private float minRadius;
+    private  float maxRadius;
+    private Color currentColor;
+    private Color targetColor;
+    private Color startColor;
     float[] vertices;
     short[] indices;
     private CatmullRomSpline<Vector2> curve;
@@ -24,35 +35,42 @@ public class CatmullRomShape {
 
     private ShapeRenderer shapeRenderer;
     private PolygonSpriteBatch batch;
+    private Random random;
+    private RandomPolygon currentRandomPolygon;
+    private RandomPolygon startRandomPolygon;
+    private RandomPolygon targetRandomPolygon;
 
+    private Vector2[] currentControlPoints;
+    private Vector2[] startControlPoints; // начальные позиции контрольных точек
+    private Vector2[] targetControlPoints; // целевые позиции контрольных точек
     private PolygonRegion region;
-    private Vector2[] controlPoints;
     private Vector2 tmp;
+    private float progress = 0;
+    private float duration;
 
-    public CatmullRomShape(float controlPoints[]) {
-        for (int i = 0; i < controlPoints.length; i += 2) {
-            this.controlPoints[i].x = controlPoints[i];
-            this.controlPoints[i+1].y = controlPoints[i+1];
-        }
-        init();
-    }
 
-    public CatmullRomShape(Array<Vector2> controlPoints) {
-        this.controlPoints = new Vector2[controlPoints.size];
-        for (int i = 0; i < controlPoints.size; i++) {
-            this.controlPoints[i] = controlPoints.get(i);
-        }
-        init();
-    }
-
-    private void init() {
+    public CatmullRomShape(int sides, float minRadius, float maxRadius, float duration) {
+        this.sides = sides;
+        this.minRadius = minRadius;
+        this.maxRadius = maxRadius;
+        this.duration = duration;
+        this.startRandomPolygon = new RandomPolygon(this.sides, this.minRadius, this.maxRadius);
+        this.targetRandomPolygon = new RandomPolygon(this.sides, this.minRadius, this.maxRadius);
+        this.currentRandomPolygon = this.startRandomPolygon;
+        this.startControlPoints = this.startRandomPolygon.getVertices().toArray(new Vector2[sides]);
+        this.targetControlPoints = this.targetRandomPolygon.getVertices().toArray(new Vector2[sides]);
+        this.currentControlPoints = this.startControlPoints;
         this.vertices = new float[POINTS_NUM * 2];
         this.indices = new short[POINTS_NUM + 2];
         this.shapeRenderer = new ShapeRenderer();
         this.batch = new PolygonSpriteBatch();
-        this.curve = new CatmullRomSpline<>(this.controlPoints, true);
+        this.curve = new CatmullRomSpline<>(this.currentControlPoints, true);
+        this.random = new Random();
+        startColor = new Color(random.nextFloat(), random.nextFloat(), random.nextFloat(), 1f);
+        targetColor = new Color(random.nextFloat(), random.nextFloat(), random.nextFloat(), 1f);
+        currentColor = startColor;
     }
-    public void drawShape(Color color) {
+    public void drawShape() {
         tmp = new Vector2();
         float t = 0;
         float step = 1f / POINTS_NUM;
@@ -72,7 +90,7 @@ public class CatmullRomShape {
         indices[POINTS_NUM + 1] = 0;
 
         pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
-        pixmap.setColor(color);
+        pixmap.setColor(currentColor);
         pixmap.fill();
         texture = new Texture(pixmap);
         pixmap.dispose();
@@ -87,6 +105,35 @@ public class CatmullRomShape {
         batch.end();
     }
 
+    public void render(Interpolation interpolation) {
+        Gdx.gl.glClearColor(1, 1, 1, 1); // задает цвет очистки экрана (черный)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // очистить экран
+
+        // обновление текущего прогресса изменения
+        progress += Gdx.graphics.getDeltaTime() / duration;
+
+
+        currentColor.r = interpolation.apply(startColor.r, targetColor.r, progress);
+        currentColor.g = interpolation.apply(startColor.g, targetColor.g, progress);
+        currentColor.b = interpolation.apply(startColor.b, targetColor.b, progress);
+        currentColor.a = interpolation.apply(startColor.a, targetColor.a, progress);
+
+        if (progress > 1f) {
+            progress = 0f;
+
+            targetRandomPolygon = new RandomPolygon(sides, minRadius, maxRadius);
+            targetColor = new Color(random.nextFloat(), random.nextFloat(), random.nextFloat(), 1f);
+            targetControlPoints = targetRandomPolygon.getVertices().toArray(new Vector2[sides]);
+            startColor = currentColor;
+            startRandomPolygon = currentRandomPolygon;
+
+            for (int i = 0; i < sides; i++) {
+                currentControlPoints[i] = new Vector2(interpolation.apply(startControlPoints[i].x, targetControlPoints[i].x, progress), Interpolation.smooth2.apply(startControlPoints[i].y, targetControlPoints[i].y, progress));
+
+            }
+        }
+        drawShape();
+    }
 
     public void dispose() {
         shapeRenderer.dispose();
--- core/src/com/iclustudio/morphica/Main.java
+++ core/src/com/iclustudio/morphica/Main.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.math.Interpolation;
 
 import java.util.List;
 import java.util.Random;
@@ -14,65 +15,17 @@ public class Main extends ApplicationAdapter {
 	private CatmullRomShape shape;
 	private Random random;
 
-	private Color startColor;
-	private Color targetColor;
-
-	private Color currentColor;
-
-	private RandomPolygon startRandomPolygon;
-	private RandomPolygon targetRandomPolygon;
-	private Array<Vector2> startControlPoints; // начальные позиции контрольных точек
-	private Array<Vector2> targetControlPoints; // целевые позиции контрольных точек
-	private float progress; // текущий прогресс изменения (от 0 до 1)
-	private float duration = 1f; // продолжительность изменения (в секундах)
-
-
+	private Interpolation interpolation;
 	@Override
 	public void create() {
-		// Создайте экземпляр класса Random.
 		random = new Random();
-		// Создайте новый цвет со случайными значениями красного, зеленого и синего компонентов.
-		startColor = new Color(random.nextFloat(), random.nextFloat(), random.nextFloat(), 1f);
-		targetColor = new Color(random.nextFloat(), random.nextFloat(), random.nextFloat(), 1f);
-		currentColor = new Color();
-		startRandomPolygon = new RandomPolygon(7, 40, 450);
-		targetRandomPolygon = new RandomPolygon(7, 40, 450);
-		startControlPoints = ToArray(startRandomPolygon.getVertices());
-		targetControlPoints = ToArray(targetRandomPolygon.getVertices());
-
-		shape = new CatmullRomShape(startControlPoints);
+		shape = new CatmullRomShape(7, 40, 450, 4f);
+		interpolation = Interpolation.smooth2;
 	}
 
 	@Override
 	public void render() {
-		Gdx.gl.glClearColor(1, 1, 1, 1); // задает цвет очистки экрана (черный)
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // очистить экран
-
-		// обновление текущего прогресса изменения
-		progress += Gdx.graphics.getDeltaTime() / duration;
-
-		currentColor.r = startColor.r * (1 - progress) + targetColor.r * progress;
-		currentColor.g = startColor.g * (1 - progress) + targetColor.g * progress;
-		currentColor.b = startColor.b * (1 - progress) + targetColor.b * progress;
-		currentColor.a = startColor.a * (1 - progress) + targetColor.a * progress;
-
-		if (progress > 1f) {
-			progress = 0f;
-			// обновление целевых позиций контрольных точек для следующей анимации
-			// ...
-			targetRandomPolygon = new RandomPolygon(7, 40, 450);
-			targetControlPoints = ToArray(targetRandomPolygon.getVertices());
-			targetColor = new Color(random.nextFloat(), random.nextFloat(), random.nextFloat(), 1f);
-			startColor = currentColor;
-		}
-
-		// линейная интерполяция между начальными и целевыми позициями контрольных точек
-		for (int i = 0; i < startControlPoints.size; i++) {
-			startControlPoints.get(i).lerp(targetControlPoints.get(i), progress);
-		}
-		// отрисовываем фигуру
-
-		shape.drawShape(currentColor);
+		shape.render(interpolation);
 	}
 
 	public Array<Vector2> ToArray(List<Vector2> list) {
